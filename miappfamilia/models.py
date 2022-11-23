730df94c701e18caf87212a469ab3fb8c4f3e443
sergio Berrocoso <scberrocoso@gmail.com>
2022-11-23 16:09:23 -0300

    modificaciones

--- miappfamilia/models.py
+++ miappfamilia/models.py
@@ -5,7 +5,9 @@ class Datos_familia(models.Model):
     Nombre = models.CharField(max_length=30)
     Edad = models.IntegerField(default=0)
     F_Nacimiento = models.DateField()
-    Sexo = models.CharField(max_length=30)
+    Cargo = models.CharField(max_length=30)
+    
+    
 
 class  pantalones(models.Model): 
     Nombre = models.CharField(max_length=30)
